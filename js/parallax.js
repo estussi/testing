ba8a8e850ed5834806b819d543f1e45ae8ad1542
estussi <82211274+estussi@users.noreply.github.com>
2021-04-14 19:18:23 +0200

    Speed changed, Liked images

--- js/parallax.js
+++ js/parallax.js
@@ -58,8 +58,8 @@ $(document).ready(function() {
 function parallaxScroll(){
 	var scrolled = $(window).scrollTop();
 	$('#content').css('left',(0-(scrolled*.9))+'px');
-	$('#parallax-bg1').css('left',(0-(scrolled*.25))+'px');
-	$('#parallax-bg2').css('left',(0-(scrolled*.5))+'px');
+	$('#parallax-bg1').css('left',(0-(scrolled*.6))+'px');
+	$('#parallax-bg2').css('left',(0-(scrolled*.9))+'px');
 	$('#parallax-bg3').css('left',(0-(scrolled*.9))+'px');
 }
 
