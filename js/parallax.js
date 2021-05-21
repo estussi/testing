7f060d6ff35a6dac90f068a9dac6fcda1683b0a5
estussi <82211274+estussi@users.noreply.github.com>
2021-05-21 20:18:52 +0200

    so much fucking misery!

--- js/parallax.js
+++ js/parallax.js
@@ -130,6 +130,7 @@ $(document).ready(function() {
 function parallaxScroll(){
 	var scrolled = $(window).scrollTop();
 	$('#content').css('left',(0-(scrolled*1.25))+'px');
+	$('#getfucked').css('left',(0-(scrolled*1.25))+'px');
     $('#pizza').css('left',(0-(scrolled*.8))+'px');
     $('#ride').css('left',(0-(scrolled*-2))+'px');    
 	$('#parallax-bg1').css('left',(0-(scrolled*.5))+'px');
